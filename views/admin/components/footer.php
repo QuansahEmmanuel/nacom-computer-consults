a67256b97cf4f104d34a3db836a4a712c5b1fab2
snopijeriel <snopijeriel@gmail.com>
2025-07-20 22:34:22 +0000

    footer

--- views/admin/components/footer.php
+++ views/admin/components/footer.php
@@ -1,7 +1,8 @@
 <footer class="bg-white shadow-sm border-t border-gray-200 px-6 py-4 ">
-    <div class="flex justify-between items-center ">
-        <p class="text-sm text-gray-600">© 2024 NACOM. All rights reserved.</p>
-    </div>
+    <div class="flex justify-center items-center">
+    <p class="text-sm text-gray-600 text-center">© 2024 NACOM. All rights reserved.</p>
+</div>
+
 </footer>
 <script>
  
